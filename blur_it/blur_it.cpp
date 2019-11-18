a5b3de8dd9e8ab6408f149fc984dcc691d48b40a
edirab <edirab@yandex.ru>
2019-11-18 22:50:37 +0300

    first commit

---
+++ blur_it/blur_it.cpp
@@ -0,0 +1,40 @@
+﻿// aruco-create-markers.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <opencv2/highgui.hpp>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/aruco.hpp>
+#include <string>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+	string window_input = "Input";
+	string window_output = "Output";
+
+	namedWindow(window_input, WINDOW_AUTOSIZE);
+	namedWindow(window_output, WINDOW_AUTOSIZE);
+
+	string path = "../../sources/pic2.jpg";
+	//string path = "./pic1.jpg";
+	Mat markerImg;
+	Mat out;
+
+	markerImg = imread(path, IMREAD_COLOR);
+	imshow(window_input, markerImg);
+	//waitKey(0);
+
+	GaussianBlur(markerImg, out, Size(13, 13), 0, 0);
+
+	imshow(window_output, out);
+	waitKey(0);
+
+	imwrite(path, out);
+	return 0;
+}
+
---
+++ detect_marker/detect_marker.cpp
@@ -0,0 +1,49 @@
+﻿
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+
+
+int main(int argc, char** argv)
+{
+	int wait_time = 10;
+	VideoCapture in_video;
+	in_video.open(0);
+	//in_video.open("Video4.wmv", cv::CAP_FFMPEG);
+
+	in_video.set(CAP_PROP_FRAME_WIDTH, 1600);
+	in_video.set(CAP_PROP_FRAME_HEIGHT, 900);
+
+	Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::DICT_ARUCO_ORIGINAL);
+
+	while (in_video.grab())
+	{
+		Mat image, image_copy;
+
+		in_video.retrieve(image);
+
+		GaussianBlur(image,image_copy, Size(13, 13), 0, 0);
+		//image.copyTo(image_copy);
+
+		vector<int> ids;
+		vector<vector<Point2f> > corners;
+
+		aruco::detectMarkers(image, dictionary, corners, ids);
+
+		// if at least one marker detected
+		if (ids.size() > 0)
+			aruco::drawDetectedMarkers(image_copy, corners, ids);
+
+		imshow("Detected markers", image_copy);
+		char key = (char)waitKey(wait_time);
+		if (key == 27)
+			break;
+	}
+
+	in_video.release();
+}
