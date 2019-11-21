79270ad23e5eb7bb7b99c2dc97a11e917be819f7
edirab <edirab@yandex.ru>
2019-11-21 23:25:32 +0300

    noise now works!

--- gaussian_noise/gaussian_noise.cpp
+++ gaussian_noise/gaussian_noise.cpp
@@ -10,6 +10,10 @@
 using namespace std;
 using namespace cv;
 
+/*
+Debug purposes only.
+Prints a part of a matrix to scecified text file.
+*/
 void printToFile(Mat img, bool truncated = false, string filename = "data.txt") {
 	ofstream out(filename);
 
@@ -33,6 +37,28 @@ void printToFile(Mat img, bool truncated = false, string filename = "data.txt") 
 	}
 }
 
+/*
+Adding gaussian noize to given image
+Returns its copy.
+*/
+Mat addNoize(Mat input, int mean, int std) {
+
+	Mat output = input.clone();
+	Mat noize(input.size(), input.type());
+
+	Scalar Mean(mean, mean, mean);
+	Scalar Std(std, std, std);
+
+	randn(noize, Mean, Std);
+
+	imshow("Noize", noize);
+	printToFile(noize, true, "noize.txt");
+
+	output += noize;
+
+	return output;
+}
+
 
 int main(int argc, char* argv[]) {
 	
@@ -40,47 +66,14 @@ int main(int argc, char* argv[]) {
 	Mat input = imread("F:/Университет/9sem/С++/code/aruco-create-markers/sources/pic1.jpg");
 	imshow("Initial", input);
 
-	Mat noise(input.size(), input.type());
-	//cout << to_string(input.type());
-
-	randn(noise, 0, 0.1); //mean and variance
-
-	/*
-	Mat3b img(3, 4, Vec3b(15, 14, 12));
-	randu(img, Scalar(0, 0, 0), Scalar(256, 256, 256));
-
-	// Save to Hex ASCII
-	ofstream out("data.txt");
-	for (int r = 0; r < img.rows; ++r) {
-		for (int c = 0; c < img.cols; ++c) {
-			const Vec3b& v = img(r, c);
-			out << "(";
-			out << uppercase << setw(4) << setfill(' ') << int(v[0]);
-			out << uppercase << setw(4) << setfill(' ') << int(v[1]);
-			out << uppercase << setw(4) << setfill(' ') << int(v[2]);
-			out << "), ";
-		}
-		out << endl;
-	}
-	*/
-
-
-	Mat noised = input.clone();
-	noised += noise;
+	Mat noised = addNoize(input, 0, 60);
+	//noised += noise;
 
 	printToFile(input, true, "input.txt");
-	printToFile(noise, true, "noise.txt");
 	printToFile(noised, true, "noised.txt");
-
-	Mat  clamp_1 = Mat::ones(noised.size(), noised.type());
-	Mat  clamp_0 = Mat::zeros(noised.size(), noised.type());
-	noised = max(noised, 0);
-	noised = min(noised, 1);
 
 	imshow("Noised", noised);
 	imwrite("F:/Университет/9sem/С++/code/aruco-create-markers/sources/pic1_noised.jpg", noised);
-
-	imwrite("F:/Университет/9sem/С++/code/aruco-create-markers/sources/noise.jpg", noise);
 
 	waitKey(0);
 
